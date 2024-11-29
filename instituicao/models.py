8bd091f4d9df532d472a58ef8cbdf30aba2bc5f9
devwenderson <wend.devdesigner@gmail.com>
2024-11-29 15:49:06 -0300

    wip: adiciona anotações

--- instituicao/models.py
+++ instituicao/models.py
@@ -6,5 +6,63 @@ fazer um model pra cada?
 
 * fazer instituicao um model só e salvar dados em cookies ate chegar no form de missao
 
+Campos:
+    Identificação:
+    - CNPJ
+    - Nome da instituicao
+    - Sigla da instituicao
+    - Tipo de estabelecimento
+    - Razão social  
+    - País
+    - Idioma
+
+    Situação cadastral:
+    - Informe  a situação 
+    - Data da situação cadastral
+    - Motivo
+
+    Situação especial:
+    - Informe a situação 
+    - Data da situação especial
+
+    Informações especiais:
+    - Optante pelo o simples
+    - Data de início
+    - Data de fim
+    - optante pelo MEI
+
+    Sócios:
+    - Tipo de sócio
+    - CPF
+    - Nome
+    - Qualificação 
+    - Data de inclusão na Sociedade
+
+    Representante legal:
+    - CPF
+    - nome 
+    - qualificação
+
+    Endereço:
+    - CEP
+    - Tipo de CEP
+    - Endereço
+    - Complemento
+    - Bairro    
+    - UF/Cidade
+
+    Contato:
+    - DDD
+    - Telefone 1
+    - Ramal
+    - Telefone 2
+    - Ramal
+    - Fax  
+    - E-mail institucional
+    - Website
+
+    Classificação:
+    - Natureza jurídica
+
 
 """
